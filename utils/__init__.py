8c3fa8673433cb87150727d049e7282e55b3e633
Jeremiah Iyamabo <jiyamabo@users.noreply.github.com>
2022-08-29 15:12:24 -0300

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from utils.perp5Scale import perp5Scale
-from utils.perp105scale import perp105Scale
-from utils.electric5scale import electric5Scale
-from utils.energy5scale import energy5scale
-from utils.genderscale import genderscale
